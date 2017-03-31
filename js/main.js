4a911baf0edcdcb58f364d8889e7d7b775bdf83e
Raiper34 <raipergm34@gmail.com>
2017-03-31 18:41:42 +0200

    date from to

--- js/main.js
+++ js/main.js
@@ -5,27 +5,77 @@
  * @Website: http://www.raiper34.net
  */
 
+var date; //date
+var pickerFrom;
+var pickerTo;
+
 $(document).ready(function ()
 {
-    var $input = $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 17, // Creates a dropdown of 15 years to control year
+    //Initialize Datepickers
+    var $inputFrom = $('#datepickerFrom').pickadate({
+        selectMonths: true,
+        selectYears: 34,
         format: 'dd.mm.yyyy'
     });
-    var picker = $input.pickadate('picker');
-    picker.set('select', new Date());
+    var $inputTo = $('#datepickerTo').pickadate({
+        selectMonths: true,
+        selectYears: 34,
+        format: 'dd.mm.yyyy'
+    });
+    pickerFrom = $inputFrom.pickadate('picker');
+    pickerFrom.set('select', new Date());
+    pickerTo = $inputTo.pickadate('picker');
+    pickerTo.set('select', new Date());
 
+    //Hide progress
+    $('#progress').hide();
+
+    /**
+     * Handle button download click
+     */
     $('#downloadButton').click(function ()
     {
-        $.ajax({
-            type: "GET",
-            url: "http://www.cnb.cz/cs/financni_trhy/skd/statistika_skd/denni.txt",
-            data: {'date': picker.get('select', 'dd.mm.yyyy')},
-            success: function (result)
-            {
-                var blob = new Blob([result], {type: "text/plain;charset=utf-8"}); //to write to txt file
-                saveAs(blob, "cbs_stat_" + picker.get('select', 'yyyy-mm-dd') + ".txt");
-            }
-        });
+        output = "";
+        $('#progress').show();
+        $('#downloadButton').addClass('disabled');
+        $('#datepickerFrom').attr('disabled', 'disabled');
+        $('#datepickerTo').attr('disabled', 'disabled');
+        date = new Date(pickerFrom.get('select', 'yyyy/mm/dd'));
+        var dateString = ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+        ajaxCall(dateString);
     });
+
 });
+
+/**
+ * Make ajjax call and call resursively until date to is processed
+ * @param string date which want to download data
+ */
+function ajaxCall(dateString)
+{
+    $('#progressText').text("Working on date: " + dateString);
+    $.ajax({
+        type: "GET",
+        url: "http://www.cnb.cz/cs/financni_trhy/skd/statistika_skd/denni.txt",
+        data: {'date': dateString},
+        success: function (result)
+        {
+            output += result + "\n";
+            if (date < new Date(pickerTo.get('select', 'yyyy/mm/dd'))) //it is not date to, we call this function again and increment day
+            {
+                date.setDate(date.getDate() + 1);
+                var dateString = ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+                ajaxCall(dateString);
+            } 
+            else //all dates , from date to date was processed now, we can download file
+            {
+                $('#progress').hide();
+                $('#downloadButton').removeClass('disabled');
+                $('#datepickerFrom').removeAttr('disabled');
+                $('#datepickerTo').removeAttr('disabled');
+                var blob = new Blob([output], {type: "text/plain;charset=utf-8"}); //to write to txt file
+                saveAs(blob, "cbs_stat_from" + pickerFrom.get('select', 'yyyy-mm-dd') + "to" + pickerTo.get('select', 'yyyy-mm-dd') + ".txt");
+            }
+        }
+    });
+}
